ca2466cec737b0204bc39bd197a1396f3928c91a
Naran Torguud <ntorguud@gmail.com>
2021-05-08 23:51:02 -0700

    Model commit

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ClubType, Product, Review
 
 # Register your models here.
+
+admin.site.register(ClubType)
+admin.site.register(Product)
+admin.site.register(Review)
+
+
+
