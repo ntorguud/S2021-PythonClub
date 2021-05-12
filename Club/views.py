db1a7366a8bbf5bbd3cd2e577df93d58bf97aa8c
Naran Torguud <ntorguud@gmail.com>
2021-05-11 22:04:14 -0700

    Product view commit

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product, ClubType, Review
 
 # Create your views here.
 def index(request):
     return render (request, 'Club/index.html')
+
+def products(request):
+    product_list = Product.objects.all()
+    return render(request, 'Club/products.html', {'products_list': product_list})
