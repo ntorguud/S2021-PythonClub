e83018c99b0da067f65a5981d563d10b63c447f4
Naran Torguud <ntorguud@gmail.com>
2021-05-16 16:33:56 -0700

    Add URL for Resource

--- Club/urls.py
+++ Club/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('members/', views.members, name='members'),
+    path('resource/', views.resource, name='resource'),
 ]
