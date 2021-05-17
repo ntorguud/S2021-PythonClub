87943097c6be245bb1ec466d7773ad2d7cecd3aa
Naran Torguud <ntorguud@gmail.com>
2021-05-16 22:05:05 -0700

    Import User

--- Club/views.py
+++ Club/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Meeting, MeetingMinute, Resource, Event
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render (request, 'Club/index.html')
 
 def members(request):
-    members_list = Meeting.objects.all()
+    members_list = User.objects.all()
     return render(request, 'Club/members.html', {'members_list': members_list})
 
 def resource(request):
