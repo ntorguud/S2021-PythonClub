aab8bbe935147091851b3d73e91aba1921f9d146
Naran Torguud <ntorguud@gmail.com>
2021-06-08 13:56:38 -0700

    Authorization Commit

--- Club/views.py
+++ Club/views.py
@@ -3,6 +3,7 @@ from .models import Meeting, MeetingMinute, Resource, Event
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from .forms import MeetingForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -24,6 +25,7 @@ def meetingDetail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, 'Club/meetingdetail.html', {'meeting' : meeting})
 
+@login_required
 def newMeeting(request):
     form=MeetingForm
 
@@ -37,6 +39,8 @@ def newMeeting(request):
         form=MeetingForm()
     return render(request, 'Club/newmeeting.html', {'form': form})
 
+def loginmessage(request):
+    return render(request, 'Club/loginmessage.html')
 
-
-
+def logoutmessage(request):
+    return render(request, 'Club/logoutmessage.html')
