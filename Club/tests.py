67137ef8254c7c8e85f6ca521d1d376f3a179c2b
Naran Torguud <ntorguud@gmail.com>
2021-06-13 23:29:51 -0700

    Tests New Meeting Authentication test

--- Club/tests.py
+++ Club/tests.py
@@ -9,27 +9,27 @@ from django.urls import reverse_lazy, reverse
 # Create your tests here.
 class MeetingTest(TestCase):
     def setUp(self):
-        self.type=Meeting(typename='Discord Server')
+        self.meetingtitle=Meeting(meetingtitle='Python Club Discord Server')
         self.user=User(username='Tuul')
-        self.meeting=Meeting(meetingtitle='Python Club Discord Server', meetingdate=datetime('6/07/2021'), meetingtime=time('12.00pm'), location='Seattle, WA', agenda='meet up')
+        self.meeting=Meeting(meetingdate=datetime('6/07/2021'), meetingtime=time('12.00pm'), location='Seattle, WA', agenda='meet up')
 
     def test_typestring(self):
-        self.assertEqual(str(self.type), 'Python Club Discord Server')
+        self.assertEqual(str(self.title), 'Python Club Discord Server')
 
     def test_tablename(self):
         self.assertEqual(str(Meeting.meta.db_table), 'meeting')
 
 class MeetingMinuteTest(TestCase):
     def setUp(self):
-        self.type=MeetingMinute(typename='meetingminutes')
+        self.meetingid=MeetingMinute(meetingid='01')
         self.user=User(username='Jenny')
-        self.meetingminute=MeetingMinute(meetingid='01', attendance='voluntary', minutestext='Welcome')
+        self.meetingminute=MeetingMinute(attendance='voluntary', minutestext='Welcome')
     
 class ResourceTest(TestCase):
     def setUp(self):
-        self.type=Resource(typename='resources')
+        self.resourcetype=Resource(resourcetype='url')
         self.user=User(userid='Jack')
-        self.resource=Resource(resourcename='Python.org Community', resourcetype='url', resourceurl='https://www.python.org/community/', dateentered=datetime('06/07/2021'), description='support')
+        self.resource=Resource(resourcename='Python.org Community', resourceurl='https://www.python.org/community/', dateentered=datetime('06/07/2021'), description='support')
 
 class NewMeetingForm(TestCase):
     #valid form data
@@ -60,8 +60,8 @@ class NewMeetingForm(TestCase):
 class New_Meeting_Authentication_test(TestCase):
     def setUp(self):
         self.test_user=User.objects.create_user(username='testTuul', password='Mongolia@123')
-        self.type=Meeting.objects.create(typename='Discord Server')
-        self.meeting=Meeting.objects.create(meetingtitle='Python Club Discord Server', meetingdate=datetime('6/07/2021'), meetingtime=time('12.00pm'), location='Seattle, WA', agenda='meet up', user=self.test_user)
+        self.meetingtitle=Meeting.objects.create(meetingtitle='Python Club Discord Server')
+        self.meeting=Meeting.objects.create(meetingdate=datetime('6/07/2021'), meetingtime=time('12.00pm'), location='Seattle, WA', agenda='meet up', user=self.test_user)
 
     def test_redirect_if_not_logged_in(self):
         response=self.client.get(reverse('newmeeting'))
